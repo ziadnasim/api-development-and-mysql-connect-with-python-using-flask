78600bf6a83f6ca0b257d51e1a9663933ea7d66b
Adnan Ahmed Khan <iamkhanadnan@gmail.com>
2020-04-14 19:27:22 +0600

    Adds procfile for heroku

--- api_hit.py
+++ api_hit.py
@@ -1,36 +1,41 @@
-from flask import request
+from flask import request, Flask
 import pymysql
-import flask
-import json
 from flask import jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
+
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+
 @app.route('/locations', methods=['GET'])
 def home1():
-    conn = pymysql.connect(host="64.188.2.228", user="odvutcom_apiTest", passwd="remoteTest123!@#", db="odvutcom_apiTest")
-    #conn = pymysql.connect(host="remotemysql.com", user="u9PE2LncVJ", passwd="EDajKwnNsd",db="u9PE2LncVJ")
+    # conn = pymysql.connect(host="64.188.2.228", user="odvutcom_apiTest", passwd="remoteTest123!@#",
+    #                        db="odvutcom_apiTest")
+    conn = pymysql.connect(host="remotemysql.com", user="u9PE2LncVJ", passwd="EDajKwnNsd",db="u9PE2LncVJ")
     myCursor = conn.cursor()
     myCursor.execute('select * from donation')
     result = myCursor.fetchall()
     return jsonify(result)
 
+
 @app.route('/add_location', methods=['POST'])
 def add_loc():
-    conn = pymysql.connect(host="64.188.2.228", user="odvutcom_apiTest", passwd="remoteTest123!@#", db="odvutcom_apiTest")
-    #conn = pymysql.connect(host="remotemysql.com", user="u9PE2LncVJ", passwd="EDajKwnNsd",db="u9PE2LncVJ")
+    # conn = pymysql.connect(host="64.188.2.228", user="odvutcom_apiTest", passwd="remoteTest123!@#",
+    #                        db="odvutcom_apiTest")
+    conn = pymysql.connect(host="remotemysql.com", user="u9PE2LncVJ", passwd="EDajKwnNsd",db="u9PE2LncVJ")
     myCursor = conn.cursor()
     d_name = request.form['name']
     d_area = request.form['area']
     d_amt = request.form['amt']
-    myCursor.execute("INSERT INTO donation (donor_name,donation_area,amt_contr) VALUES (%s,%s,%s)", (d_name, d_area, d_amt))
+    myCursor.execute("INSERT INTO donation (donor_name,donation_area,amt_contr) VALUES (%s,%s,%s)",
+                     (d_name, d_area, d_amt))
     conn.commit()
     conn.close()
     return d_name
+
 
 app.run()
