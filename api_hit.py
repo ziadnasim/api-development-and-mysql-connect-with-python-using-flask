b1911f5bfbe6b2039611999744c6ad62d7190f1e
Ziad Nasim <ziad30297@bd.bracbank.com>
2020-04-19 16:48:56 +0600

    Adds cors

--- api_hit.py
+++ api_hit.py
@@ -4,9 +4,11 @@ import flask
 import json
 from flask import jsonify
 import pymysql.cursors
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
