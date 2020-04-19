614342d4347ce488494b77923ce512ab8436ee31
Ziad Nasim <ziad30297@bd.bracbank.com>
2020-04-19 13:23:36 +0600

    updates locations

--- api_hit.py
+++ api_hit.py
@@ -16,20 +16,20 @@ def home():
 def home1():
     conn = pymysql.connect(host="remotemysql.com", user="u9PE2LncVJ", passwd="EDajKwnNsd", db="u9PE2LncVJ")
     myCursor = conn.cursor()
-    myCursor.execute('select * from donation')
+    myCursor.execute('select name, location, donation_address, donation_count, donation_amt, thana, lat, longx from donation')
     result = myCursor.fetchall()
     data = []
 
     for item in result:
         entry = {
-            'name': item[0],
-            'lcoation': item[1],
-            'donation_address': item[2],
-            'donation_count': item[3],
-            'donation_amount': item[4],
-            'thana': item[5],
-            'lat': item[6],
-            'longx': item[7],
+            'name': item[1],
+            'lcoation': item[2],
+            'donation_address': item[3],
+            'donation_count': item[4],
+            'donation_amount': item[5],
+            'thana': item[6],
+            'lat': item[7],
+            'longx': item[8],
         }
         data.append(entry)
     return jsonify(data)
@@ -46,8 +46,7 @@ def add_loc():
     d_thana = request.form['thana']
     d_lat = request.form['lat']
     d_longx = request.form['longx']
-    myCursor.execute(
-        "INSERT INTO donation (name, location, donation_address, donation_count, donation_amt, thana, lat, longx) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
+    myCursor.execute("INSERT INTO donation (name, location, donation_address, donation_count, donation_amt, thana, lat, longx) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
         (d_name, d_loc, d_area, d_count, d_amt, d_thana, d_lat, d_longx))
     conn.commit()
     conn.close()
